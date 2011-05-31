051c0c1b20a6281833786503c0aaeb9449377694
Aaron Faanes <dafrito@gmail.com>
2011-05-30 19:57:28 -0500

    Added a no-op inspector

---
+++ src/com/dafrito/rfe/inspect/NoopInspector.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.dafrito.rfe.inspect;
+
+/**
+ * An {@link Inspector} that does nothing.
+ * 
+ * @author Aaron Faanes
+ * @param <T>
+ *            the common supertype of inspected values
+ */
+public class NoopInspector<T> implements Inspector<T> {
+
+	@Override
+	public void field(String name, T value) {
+	}
+
+	@Override
+	public void value(T value) {
+	}
+
+	@Override
+	public Inspector<T> group(String groupName) {
+		return this;
+	}
+
+	@Override
+	public void comment(String note) {
+	}
+
+	@Override
+	public void close() {
+	}
+
+}
