07bde7ab46a2dd2ec8ab88fa48e2891e8a057e5c
Aaron Faanes <dafrito@gmail.com>
2011-06-02 01:10:47 -0500

    Doc'd ScriptConvertible

--- src/com/dafrito/rfe/script/ScriptConvertible.java
+++ src/com/dafrito/rfe/script/ScriptConvertible.java
@@ -1,5 +1,15 @@
 package com.dafrito.rfe.script;
 
+/**
+ * Indicate that this object is convertible to a scriptable object. We actually
+ * use this interface to indicate "convertibility" in both directions, otherwise
+ * I'd change the type parameter to something more specific.
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <T>
+ *            the type of the converted object
+ */
 public interface ScriptConvertible<T> {
 	public T convert();
 }
