fe86b92541d3578967ecb390ebd7b8fa5da32d50
Aaron Faanes <dafrito@gmail.com>
2011-06-04 21:47:50 -0500

    RIP#equals: Added short-circuit for obj == this

--- src/com/dafrito/rfe/geom/RiffIntersectionPoint.java
+++ src/com/dafrito/rfe/geom/RiffIntersectionPoint.java
@@ -31,6 +31,9 @@ public class RiffIntersectionPoint {
 
 	@Override
 	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
 		if (!(obj instanceof RiffIntersectionPoint)) {
 			return false;
 		}
