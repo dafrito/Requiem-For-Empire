a266b73a0e6ec6929076b74ac9d8889f9d775e53
Aaron Faanes <dafrito@gmail.com>
2011-06-06 05:21:14 -0500

    Added NoopScriptFunction

---
+++ src/com/dafrito/rfe/script/values/NoopScriptFunction.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package com.dafrito.rfe.script.values;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.dafrito.rfe.script.exceptions.Exception_Nodeable;
+import com.dafrito.rfe.script.operations.ScriptExecutable;
+import com.dafrito.rfe.script.parsing.Referenced;
+import com.dafrito.rfe.script.parsing.ScriptKeywordType;
+
+/**
+ * An immutable {@link ScriptFunction} that does nothing when called. This
+ * function must not be modified; attempts to modify it will result in
+ * {@link UnsupportedOperationException} errors.
+ * 
+ * @author Aaron Faanes
+ * 
+ */
+public class NoopScriptFunction implements ScriptFunction {
+
+	private static final NoopScriptFunction INSTANCE = new NoopScriptFunction();
+
+	public static NoopScriptFunction instance() {
+		return INSTANCE;
+	}
+
+	private NoopScriptFunction() {
+	}
+
+	@Override
+	public void addExpression(ScriptExecutable exp) throws Exception_Nodeable {
+		throw new UnsupportedOperationException("Noop function does not accept expressions");
+	}
+
+	@Override
+	public void addExpressions(List<ScriptExecutable> list) throws Exception_Nodeable {
+		throw new UnsupportedOperationException("Noop function does not accept expressions");
+	}
+
+	@Override
+	public boolean areParametersConvertible(List<ScriptValue> list) {
+		return true;
+	}
+
+	@Override
+	public boolean areParametersEqual(List<ScriptValue> list) {
+		return true;
+	}
+
+	@Override
+	public void execute(Referenced ref, List<ScriptValue> valuesGiven) throws Exception_Nodeable {
+		// Do nothing
+	}
+
+	@Override
+	public List<ScriptValue> getParameters() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public ScriptKeywordType getPermission() {
+		return ScriptKeywordType.PUBLIC;
+	}
+
+	@Override
+	public ScriptValueType getReturnType() {
+		return ScriptValueType.VOID;
+	}
+
+	@Override
+	public ScriptValue getReturnValue() {
+		return new ScriptValue_Null(null);
+	}
+
+	@Override
+	public boolean isAbstract() {
+		return false;
+	}
+
+	@Override
+	public boolean isStatic() {
+		return true;
+	}
+
+	@Override
+	public void setReturnValue(Referenced element, ScriptValue value) throws Exception_Nodeable {
+		throw new UnsupportedOperationException("Return value must not be set");
+	}
+
+}
