2eb20021f35fc514848b106bd1f2fe59a7628e38
Aaron Faanes <dafrito@gmail.com>
2011-06-06 21:06:38 -0500

    Doc'd a little more regarding Nodeable

--- src/com/dafrito/rfe/inspect/Nodeable.java
+++ src/com/dafrito/rfe/inspect/Nodeable.java
@@ -2,9 +2,12 @@ package com.dafrito.rfe.inspect;
 
 /**
  * A class that can manually nodify itself.
+ * <p>
+ * {@link Inspectable} should be used in preference to this class, as I'm trying
+ * to phase {@link Nodeable} implementations out.
  * 
  * @author Aaron Faanes
- * 
+ * @see Inspectable
  */
 public interface Nodeable {
 	public void nodificate();
