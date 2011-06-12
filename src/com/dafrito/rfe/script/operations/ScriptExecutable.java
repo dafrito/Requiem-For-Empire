3119b61df317902179443d4ae2ea207a6deed5cb
Aaron Faanes <dafrito@gmail.com>
2011-06-12 02:56:23 -0500

    Minor whitespace

--- src/com/dafrito/rfe/script/operations/ScriptExecutable.java
+++ src/com/dafrito/rfe/script/operations/ScriptExecutable.java
@@ -4,7 +4,6 @@ import com.dafrito.rfe.script.exceptions.ScriptException;
 import com.dafrito.rfe.script.parsing.ScriptElement;
 import com.dafrito.rfe.script.values.ScriptValue;
 
-
 public interface ScriptExecutable {
 	// ScriptExecutable implementation
 	public ScriptValue execute() throws ScriptException;
