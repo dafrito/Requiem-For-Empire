d89edb33201ded3b7544f5840af4d6d1c6e170e7
Aaron Faanes <dafrito@gmail.com>
2011-06-12 05:52:29 -0500

    Lots more sorting in Conversions

--- src/com/dafrito/rfe/script/Conversions.java
+++ src/com/dafrito/rfe/script/Conversions.java
@@ -50,44 +50,80 @@ public final class Conversions {
 		throw new AssertionError("Instantiation is not allowed");
 	}
 
+	public static <T> T convert(ScriptEnvironment env, ScriptConvertible<T> object) throws ScriptException {
+		return object.convert(env);
+	}
+
 	public static ScriptValue_Boolean convert(ScriptEnvironment env, boolean value) throws ScriptException {
 		return new ScriptValue_Boolean(env, value);
+	}
+
+	public static Boolean getBoolean(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Boolean) convert(env, obj);
+	}
+
+	public static ScriptValue_Boolean getRiffBoolean(ScriptEnvironment env, boolean value) throws ScriptException {
+		return convert(env, value);
 	}
 
 	public static ScriptValue_Numeric convert(ScriptEnvironment env, short num) throws ScriptException {
 		return new ScriptValue_Numeric(env, Short.valueOf(num));
 	}
 
+	public static Short getShort(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Short) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.SHORT));
+	}
+
+	public static ScriptValue_Numeric getRiffShort(ScriptEnvironment env, short value) throws ScriptException {
+		return convert(env, value);
+	}
+
 	public static ScriptValue_Numeric convert(ScriptEnvironment env, int num) throws ScriptException {
 		return new ScriptValue_Numeric(env, Integer.valueOf(num));
+	}
+
+	public static Integer getInteger(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Integer) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.INT));
+	}
+
+	public static ScriptValue_Numeric getRiffInt(ScriptEnvironment env, int value) throws ScriptException {
+		return convert(env, value);
 	}
 
 	public static ScriptValue_Numeric convert(ScriptEnvironment env, long num) throws ScriptException {
 		return new ScriptValue_Numeric(env, Long.valueOf(num));
 	}
 
+	public static Long getLong(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Long) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.LONG));
+	}
+
+	public static ScriptValue_Numeric getRiffLong(ScriptEnvironment env, long value) throws ScriptException {
+		return convert(env, value);
+	}
+
 	public static ScriptValue_Numeric convert(ScriptEnvironment env, float num) throws ScriptException {
 		return new ScriptValue_Numeric(env, Float.valueOf(num));
+	}
+
+	public static Float getFloat(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Float) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.FLOAT));
+	}
+
+	public static ScriptValue_Numeric getRiffFloat(ScriptEnvironment env, float value) throws ScriptException {
+		return convert(env, value);
 	}
 
 	public static ScriptValue_Numeric convert(ScriptEnvironment env, double num) throws ScriptException {
 		return new ScriptValue_Numeric(env, Double.valueOf(num));
 	}
 
-	public static ScriptValue_Numeric convert(ScriptEnvironment env, Number num) throws ScriptException {
-		return new ScriptValue_Numeric(env, num);
+	public static Double getDouble(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Double) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.DOUBLE));
 	}
 
-	public static FauxTemplate_Color convert(ScriptEnvironment env, Color color) throws ScriptException {
-		FauxTemplate_Color fauxColor = new FauxTemplate_Color(env, ScriptValueType.createType(env, FauxTemplate_Color.COLORSTRING));
-		fauxColor.setColor(color);
-		return fauxColor;
-	}
-
-	public static FauxTemplate_List convert(ScriptEnvironment env, List<ScriptValue> elements) throws ScriptException {
-		FauxTemplate_List list = new FauxTemplate_List(env, ScriptValueType.createType(env, FauxTemplate_List.LISTSTRING));
-		list.setList(elements);
-		return list;
+	public static ScriptValue_Numeric getRiffDouble(ScriptEnvironment env, double value) throws ScriptException {
+		return convert(env, value);
 	}
 
 	public static Object convert(ScriptEnvironment env, Object object) throws ScriptException {
@@ -110,32 +146,88 @@ public final class Conversions {
 		return convert(env, (ScriptConvertible<?>) ((ScriptValue) object).getValue());
 	}
 
-	public static <T> T convert(ScriptEnvironment env, ScriptConvertible<T> object) throws ScriptException {
-		return object.convert(env);
+	public static Object getObject(ScriptEnvironment env, Object obj) throws ScriptException {
+		return convert(env, obj);
+	}
+
+	public static ScriptValue_Numeric convert(ScriptEnvironment env, Number num) throws ScriptException {
+		return new ScriptValue_Numeric(env, num);
+	}
+
+	public static Number getNumber(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Number) convert(env, obj);
+	}
+
+	public static ScriptValue_Numeric getRiffNumber(ScriptEnvironment env, Number value) throws ScriptException {
+		return convert(env, value);
+	}
+
+	public static FauxTemplate_Color convert(ScriptEnvironment env, Color color) throws ScriptException {
+		FauxTemplate_Color fauxColor = new FauxTemplate_Color(env, ScriptValueType.createType(env, FauxTemplate_Color.COLORSTRING));
+		fauxColor.setColor(color);
+		return fauxColor;
+	}
+
+	public static Color getColor(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Color) convert(env, obj);
+	}
+
+	public static FauxTemplate_Color getRiffColor(ScriptEnvironment env, Color color) throws ScriptException {
+		return convert(env, color);
+	}
+
+	public static FauxTemplate_Color getRiffColor(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (FauxTemplate_Color) convert(env, obj);
+	}
+
+	public static FauxTemplate_List convert(ScriptEnvironment env, List<ScriptValue> elements) throws ScriptException {
+		FauxTemplate_List list = new FauxTemplate_List(env, ScriptValueType.createType(env, FauxTemplate_List.LISTSTRING));
+		list.setList(elements);
+		return list;
 	}
 
 	public static ScriptValue_String convert(ScriptEnvironment env, String string) throws ScriptException {
 		return new ScriptValue_String(env, string);
 	}
 
+	// Script->Engine conversion functions
+	public static String getString(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (String) convert(env, obj);
+	}
+
+	public static ScriptValue_String getRiffString(ScriptEnvironment env, String value) throws ScriptException {
+		return convert(env, value);
+	}
+
 	public static Ace getAce(ScriptEnvironment env, Object obj) throws ScriptException {
 		return (Ace) convert(env, obj);
+	}
+
+	// Engine->Script Conversion functions
+	public static FauxTemplate_Ace getRiffAce(ScriptEnvironment env, Ace ace) throws ScriptException {
+		FauxTemplate_Ace wrappedAce = new FauxTemplate_Ace(env, ScriptValueType.createType(env, FauxTemplate_Ace.ACESTRING));
+		wrappedAce.setAce(ace);
+		return wrappedAce;
 	}
 
 	public static Archetype getArchetype(ScriptEnvironment env, Object obj) throws ScriptException {
 		return (Archetype) convert(env, obj);
 	}
 
+	public static FauxTemplate_Archetype getRiffArchetype(ScriptEnvironment env, Archetype archetype) throws ScriptException {
+		FauxTemplate_Archetype wrapped = new FauxTemplate_Archetype(env, ScriptValueType.createType(env, FauxTemplate_Archetype.ARCHETYPESTRING));
+		wrapped.setArchetype(archetype);
+		return wrapped;
+	}
+
 	public static Asset getAsset(ScriptEnvironment env, Object obj) throws ScriptException {
 		return (Asset) convert(env, obj);
 	}
 
-	public static Boolean getBoolean(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Boolean) convert(env, obj);
-	}
-
-	public static Color getColor(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Color) convert(env, obj);
+	public static FauxTemplate_Asset getRiffAsset(ScriptEnvironment env, Asset asset) throws ScriptException {
+		FauxTemplate_Asset wrapped = new FauxTemplate_Asset(env, env.getTemplate(FauxTemplate_Asset.ASSETSTRING).getType());
+		wrapped.setAsset(asset);
+		return wrapped;
 	}
 
 	// Conversion assistance functions
@@ -147,99 +239,38 @@ public final class Conversions {
 		return (DiscreteRegion) convert(env, obj);
 	}
 
-	public static Double getDouble(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Double) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.DOUBLE));
-	}
-
-	public static InterfaceElement getElement(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (InterfaceElement) convert(env, obj);
-	}
-
-	public static Float getFloat(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Float) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.FLOAT));
-	}
-
-	public static GraphicalElement getGraphicalElement(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (GraphicalElement) convert(env, obj);
-	}
-
-	public static Integer getInteger(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Integer) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.INT));
-	}
-
-	public static List<ScriptValue> getList(Object obj) throws ScriptException {
-		return ((FauxTemplate_List) getCoreValue(obj)).getList();
-	}
-
-	public static Long getLong(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Long) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.LONG));
-	}
-
-	public static Number getNumber(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Number) convert(env, obj);
-	}
-
-	public static Object getObject(ScriptEnvironment env, Object obj) throws ScriptException {
-		return convert(env, obj);
-	}
-
-	public static Point getPoint(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Point) convert(env, obj);
-	}
-
-	// Engine->Script Conversion functions
-	public static FauxTemplate_Ace getRiffAce(ScriptEnvironment env, Ace ace) throws ScriptException {
-		FauxTemplate_Ace wrappedAce = new FauxTemplate_Ace(env, ScriptValueType.createType(env, FauxTemplate_Ace.ACESTRING));
-		wrappedAce.setAce(ace);
-		return wrappedAce;
-	}
-
-	public static FauxTemplate_Archetype getRiffArchetype(ScriptEnvironment env, Archetype archetype) throws ScriptException {
-		FauxTemplate_Archetype wrapped = new FauxTemplate_Archetype(env, ScriptValueType.createType(env, FauxTemplate_Archetype.ARCHETYPESTRING));
-		wrapped.setArchetype(archetype);
-		return wrapped;
-	}
-
-	public static FauxTemplate_Asset getRiffAsset(ScriptEnvironment env, Asset asset) throws ScriptException {
-		FauxTemplate_Asset wrapped = new FauxTemplate_Asset(env, env.getTemplate(FauxTemplate_Asset.ASSETSTRING).getType());
-		wrapped.setAsset(asset);
-		return wrapped;
-	}
-
-	public static ScriptValue_Boolean getRiffBoolean(ScriptEnvironment env, boolean value) throws ScriptException {
-		return convert(env, value);
-	}
-
-	public static FauxTemplate_Color getRiffColor(ScriptEnvironment env, Color color) throws ScriptException {
-		return convert(env, color);
-	}
-
-	public static FauxTemplate_Color getRiffColor(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (FauxTemplate_Color) convert(env, obj);
-	}
-
 	public static FauxTemplate_DiscreteRegion getRiffDiscreteRegion(ScriptEnvironment env, DiscreteRegion region) throws ScriptException {
 		FauxTemplate_DiscreteRegion wrapped = new FauxTemplate_DiscreteRegion(env, env.getTemplate(FauxTemplate_DiscreteRegion.DISCRETEREGIONSTRING).getType());
 		wrapped.setRegion(region);
 		return wrapped;
 	}
 
-	public static ScriptValue_Numeric getRiffDouble(ScriptEnvironment env, double value) throws ScriptException {
-		return convert(env, value);
+	public static InterfaceElement getElement(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (InterfaceElement) convert(env, obj);
+	}
+
+	public static GraphicalElement getGraphicalElement(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (GraphicalElement) convert(env, obj);
+	}
+
+	public static List<ScriptValue> getList(Object obj) throws ScriptException {
+		return ((FauxTemplate_List) getCoreValue(obj)).getList();
+	}
+
+	public static Point getPoint(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Point) convert(env, obj);
+	}
+
+	public static FauxTemplate_Point getRiffPoint(ScriptEnvironment env, Point point) throws ScriptException {
+		FauxTemplate_Point wrapped = new FauxTemplate_Point(env);
+		wrapped.setPoint(point);
+		return wrapped;
 	}
 
 	public static FauxTemplate_InterfaceElement getRiffElement(ScriptEnvironment env, InterfaceElement elem) throws ScriptException {
 		FauxTemplate_InterfaceElement wrapped = new FauxTemplate_InterfaceElement(env, ScriptValueType.createType(env, FauxTemplate_InterfaceElement.INTERFACEELEMENTSTRING));
 		wrapped.setElement(elem);
 		return wrapped;
-	}
-
-	public static ScriptValue_Numeric getRiffFloat(ScriptEnvironment env, float value) throws ScriptException {
-		return convert(env, value);
-	}
-
-	public static ScriptValue_Numeric getRiffInt(ScriptEnvironment env, int value) throws ScriptException {
-		return convert(env, value);
 	}
 
 	public static FauxTemplate_Line getRiffLine(ScriptEnvironment env, GraphicalElement_Line line) {
@@ -253,38 +284,20 @@ public final class Conversions {
 		return convert(env, list);
 	}
 
-	public static ScriptValue_Numeric getRiffLong(ScriptEnvironment env, long value) throws ScriptException {
-		return convert(env, value);
-	}
-
-	public static ScriptValue_Numeric getRiffNumber(ScriptEnvironment env, Number value) throws ScriptException {
-		return convert(env, value);
-	}
-
 	public static FauxTemplate_Path getRiffPath(ScriptEnvironment env, Point_Path path) throws ScriptException {
 		FauxTemplate_Path wrapped = new FauxTemplate_Path(env, ScriptValueType.createType(env, FauxTemplate_Path.PATHSTRING));
 		wrapped.setPoint(path);
 		return wrapped;
 	}
 
-	public static FauxTemplate_Point getRiffPoint(ScriptEnvironment env, Point point) throws ScriptException {
-		FauxTemplate_Point wrapped = new FauxTemplate_Point(env);
-		wrapped.setPoint(point);
-		return wrapped;
+	public static Scheduler getScheduler(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Scheduler) convert(env, obj);
 	}
 
 	public static FauxTemplate_Scheduler getRiffScheduler(ScriptEnvironment env, Scheduler scheduler) throws ScriptException {
 		FauxTemplate_Scheduler wrapped = new FauxTemplate_Scheduler(env, ScriptValueType.createType(env, FauxTemplate_Scheduler.SCHEDULERSTRING));
 		wrapped.setScheduler(scheduler);
 		return wrapped;
-	}
-
-	public static ScriptValue_Numeric getRiffShort(ScriptEnvironment env, short value) throws ScriptException {
-		return convert(env, value);
-	}
-
-	public static ScriptValue_String getRiffString(ScriptEnvironment env, String value) throws ScriptException {
-		return convert(env, value);
 	}
 
 	public static Stylesheet getRiffStylesheet(ScriptEnvironment env, Object obj) throws ScriptException {
@@ -297,6 +310,10 @@ public final class Conversions {
 		return wrapped;
 	}
 
+	public static Terrestrial getTerrestrial(ScriptEnvironment env, Object obj) throws ScriptException {
+		return (Terrestrial) convert(env, obj);
+	}
+
 	public static FauxTemplate_Terrestrial getRiffTerrestrial(ScriptEnvironment env, Terrestrial terrestrial) throws ScriptException {
 		FauxTemplate_Terrestrial wrapped = new FauxTemplate_Terrestrial(env, ScriptValueType.createType(env, FauxTemplate_Terrestrial.TERRESTRIALSTRING));
 		wrapped.setTerrestrial(terrestrial);
@@ -307,21 +324,8 @@ public final class Conversions {
 		return (Scenario) convert(env, obj);
 	}
 
-	public static Scheduler getScheduler(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Scheduler) convert(env, obj);
-	}
-
 	public static ScriptTemplate_Abstract getSchedulerListener(Object obj) throws ScriptException {
 		return (ScriptTemplate_Abstract) getCoreValue(obj);
-	}
-
-	public static Short getShort(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Short) convert(env, ((ScriptValue) obj).castToType(null, ScriptValueType.SHORT));
-	}
-
-	// Script->Engine conversion functions
-	public static String getString(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (String) convert(env, obj);
 	}
 
 	public static Stylesheet getStylesheet(ScriptEnvironment env, Object obj) throws ScriptException {
@@ -330,9 +334,5 @@ public final class Conversions {
 
 	public static ScriptTemplate_Abstract getTemplate(Object obj) throws ScriptException {
 		return (ScriptTemplate_Abstract) ((ScriptValue) obj).getValue();
-	}
-
-	public static Terrestrial getTerrestrial(ScriptEnvironment env, Object obj) throws ScriptException {
-		return (Terrestrial) convert(env, obj);
 	}
 }
