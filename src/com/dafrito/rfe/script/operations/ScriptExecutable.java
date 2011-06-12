d502cbcb5abf39443d3eba964d1c6e624af8f75a
Aaron Faanes <dafrito@gmail.com>
2011-06-12 10:01:06 -0500

    Removed unneeded comment

--- src/com/dafrito/rfe/script/operations/ScriptExecutable.java
+++ src/com/dafrito/rfe/script/operations/ScriptExecutable.java
@@ -5,7 +5,7 @@ import com.dafrito.rfe.script.parsing.ScriptElement;
 import com.dafrito.rfe.script.values.ScriptValue;
 
 public interface ScriptExecutable {
-	// ScriptExecutable implementation
+
 	public ScriptValue execute() throws ScriptException;
 
 	public ScriptElement getDebugReference();
